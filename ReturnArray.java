fa58bab65f233274a85d8c50d5c9e680c89ccbf2
Pavani23678 <pavaniburadhakavi@gmail.com>
2024-07-23 17:41:40 +0530

    return

--- ReturnArray.java
+++ ReturnArray.java
@@ -1,11 +1,18 @@
 public class ReturnArray{
   
 public int[] printarr(int[]a){
+  
   // initialize []a here
   return a;   
     }
   public static void main(String[]args){
   ReturnArray r=new ReturnArray();
+  int a[]={1,2,3,4,5};
+  int result[]=r.printarr(a);
+  for(int j:result){
+    System.out.println(j);
+  }
+
     // call the function and print it here
   }
 }

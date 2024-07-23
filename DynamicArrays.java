61aaa86b697df0e6d0d3171cce48b7385ea06bcc
ratan-17 <rkmule2002@gmail.com>
2024-07-23 18:39:53 +0530

    Ratan

--- DynamicArrays.java
+++ DynamicArrays.java
@@ -1,5 +1,26 @@
-public class DynamicArrays{
+import java.util.Scanner;
+
+public class DynamicArrays
+{
 //create an array
   // initialize an array using scanner and for loop
   // iterate the values using for each loop
+
+  public static void main(String[] args) 
+  {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter the length of an array");
+    int arr[]=new int[sc.nextInt()];
+
+    for(int i=0;i<=arr.length-1;i++)
+    {
+      System.out.println("Enter the value for index: "+i);
+      arr[i]=sc.nextInt();
+    }
+     for (int i : arr)
+      {
+          System.out.println(i);
+     }
+     sc.close();
+  }
 }
--- SampleArrays.java
+++ SampleArrays.java
@@ -1,4 +1,20 @@
-public class SampleArrays{
-// Declare and Initialize an Array
+public class SampleArrays
+{
+  // Declare and Initialize an Array
   // print an array using for loop
+
+   public static void main(String []args)
+   {
+    int [] ar=new int[]{11,22,33,44,55};
+
+    for (int i=0;i<=ar.length-1;i++)
+    {
+      System.out.print(ar[i]+" ");
+    }
+
+    }
+
+
 }
+
+

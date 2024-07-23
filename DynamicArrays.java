3207924701b048047f9407e13e18a2b6d2b917bc
Bharathmanne18 <bharathmanne12@gmail.com>
2024-07-23 17:25:57 +0530

    Update DynamicArrays.java

--- DynamicArrays.java
+++ DynamicArrays.java
@@ -3,3 +3,22 @@ public class DynamicArrays{
   // initialize an array using scanner and for loop
   // iterate the values using for each loop
 }
+import java.util.Scanner;
+
+public class DynamicArrays {
+public static void main(String[] args) {
+    
+    Scanner in=new Scanner(System.in);
+    int []a={10,20,30,40,50};
+    for(int i=0;i<a.length;i++){
+    System.out.println(a[i]);
+}
+int[]scan=new int[5];
+for(int i=0;i<scan.length;i++){
+scan[i]=in.nextInt();
+}
+for(int j : scan){
+System.out.println(j);
+}
+}
+}

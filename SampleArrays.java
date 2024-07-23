724dea4da9f8848b6ad5bce552ec92bad71b6d9e
Bharathmanne18 <bharathmanne12@gmail.com>
2024-07-23 17:50:06 +0530

    Update SampleArrays.java

--- SampleArrays.java
+++ SampleArrays.java
@@ -2,3 +2,11 @@ public class SampleArrays{
 // Declare and Initialize an Array
   // print an array using for loop
 }
+public class SampleArrays {
+    public static void main(String[] args) {
+        int []a={10,20,30,40,50};
+    for(int i=0;i<a.length;i++){
+        System.out.println(a[i]);
+    }
+}
+}

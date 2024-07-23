e61c48476d2d7c4cf4414c8bb8fdc19d0a8e065b
goutham120203 <gouthampaladugu12@gmail.com>
2024-07-24 00:08:54 +0530

    goutham

--- SampleArrays.java
+++ SampleArrays.java
@@ -1,4 +1,10 @@
 public class SampleArrays{
+  public static void main(String args[]){
 // Declare and Initialize an Array
+int arr[] = {1,2,3};
   // print an array using for loop
+  for(int i=0;i<arr.length;i++){
+    System.out.println("the values are"+arr[i]);
+  }
+  }
 }

7b864da0efb25a11651cb7c899285e6b2cfc68e6
2110Abhishek <agchoudhari2110@gmail.com>
2024-07-23 21:40:14 +0530

    Abhishek

--- ReturnArray.java
+++ ReturnArray.java
@@ -1,11 +1,20 @@
-public class ReturnArray{
-  
-public int[] printarr(int[]a){
-  // initialize []a here
-  return a;   
+public class ReturnArray
+{
+public int[] printarr(int[]a)
+{
+// initialize []a here
+    return a;   
     }
-  public static void main(String[]args){
-  ReturnArray r=new ReturnArray();
+    public static void main(String[]args)
+    {
+      ReturnArray r=new ReturnArray();
     // call the function and print it here
-  }
-}
+    int a[]={1,2,3,4,5};
+    r.printarr(a);
+    for (int j : a){
+    System.out.println(j);
+      }
+      // call the function and print it here
+      }
+    }
+

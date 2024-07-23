abeb41d47971b879dbd8a6351c780b48ff32d5a0
preetham0223 <sagilinoyal@gmail.com>
2024-07-23 21:12:56 +0530

    commit

--- ReturnArray.java
+++ ReturnArray.java
@@ -1,11 +1,17 @@
 public class ReturnArray{
   
 public int[] printarr(int[]a){
-  // initialize []a here
+  System.out.print("Array elements: ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
   return a;   
     }
   public static void main(String[]args){
   ReturnArray r=new ReturnArray();
-    // call the function and print it here
+  int[] arr={1,2,3,4,5};
+  int[] returnArray = r.printarr(arr);
+  System.out.println("Returned array :");
+   }
   }
-}

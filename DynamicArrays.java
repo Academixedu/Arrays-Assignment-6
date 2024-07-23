248c6615b5186a2ee16b52aa6db744fb4287a905
SriSankeerthanaGorrela <sankeerthanagorrela@gmail.com>
2024-07-23 17:34:12 +0530

    changes

--- DynamicArrays.java
+++ DynamicArrays.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
 public class DynamicArrays{
+public static void main(String args[]){
+ 
+  Scanner sc=new Scanner(System.in);
+  System.out.println("enter length of the array");
+  int length=sc.nextInt();
+  int a[]=new int[length];
+  for(int i=0;i<length;i++){
+    
+    System.out.println("enter value of index :"+i);
+    a[i]=sc.nextInt();
+  }
+  for (int j:a) {
+    System.out.println(j);
+    
+  }
+}
 //create an array
   // initialize an array using scanner and for loop
   // iterate the values using for each loop
--- SampleArrays.java
+++ SampleArrays.java
@@ -1,4 +1,13 @@
 public class SampleArrays{
 // Declare and Initialize an Array
+public static void main(String args[]){
+
+
+int a[]={1,2,3,4};
+for(int i=0;i<a.length;i++){
+  System.out.println(a[i]);
+}
   // print an array using for loop
 }
+}
+

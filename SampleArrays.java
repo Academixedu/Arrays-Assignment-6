ebfbbdbac7b1cc5f5bba5521e621b1fe45dd5780
fayazshaik515 <fayazshaik231104@gmail.com>
2024-07-23 20:18:57 +0530

    commit

--- SampleArrays.java
+++ SampleArrays.java
@@ -1,4 +1,12 @@
-public class SampleArrays{
-// Declare and Initialize an Array
-  // print an array using for loop
-}
+  public class SampleArrays {
+
+    public static void main(String[] args) {
+        // Declare and initialize an array
+        int[] array = {10, 20, 30, 40, 50};
+
+        // Print the array using a for loop
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+  }

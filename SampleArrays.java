9ff02e0b4eb9e1e5d215b622662aa329ac0a17fd
Verat-hub <venkey482thalka@gmail.com>
2024-07-24 10:15:06 +0530

    Venkat

--- SampleArrays.java
+++ SampleArrays.java
@@ -1,4 +1,14 @@
 public class SampleArrays{
 // Declare and Initialize an Array
+public static void main(String[] args) {
+  int[]a={1,2,3,4,5};
+  for(int i=0;i<=5;i++){
+    System.out.println("The entered values are: "+i);
+  }
+  for(int j:a){
+    System.out.println(j);
+  }
+
+}
   // print an array using for loop
 }

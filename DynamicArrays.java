9270210178e724c271b5edc1261666f63d9cb5e1
Manisugguna0 <subrahmanyamanikanta@gmail.com>
2024-07-24 16:49:35 +0530

    MANI

--- DynamicArrays.java
+++ DynamicArrays.java
@@ -1,5 +1,28 @@
+
+import java.util.Scanner;
+
 public class DynamicArrays{
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+  
+    System.out.println("enter the length of the array");
+    int length = scan.nextInt();
+    int a[] = new int[length];
+    for(int i = 0; i< a.length;i++)
+    {
+      System.out.println("enter the element at index= "+i);
+      a[i] = scan.nextInt();
+    }
+    System.out.println("values are ");
+    for(int j : a)
+    {
+      System.out.println(j);
+    }
+
+  }
 //create an array
   // initialize an array using scanner and for loop
   // iterate the values using for each loop
+  
+  
 }
--- ReturnArray.java
+++ ReturnArray.java
@@ -1,11 +1,18 @@
 public class ReturnArray{
   
 public int[] printarr(int[]a){
+ 
   // initialize []a here
   return a;   
     }
   public static void main(String[]args){
   ReturnArray r=new ReturnArray();
     // call the function and print it here
+    int a[]={1,2,3,4,5};
+    r.printarr(a);
+    for (int j : a){
+     System.out.println(j);
+    }
+    
   }
 }

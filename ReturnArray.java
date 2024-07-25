7f00b6588ca5a5d5d8b6d49b94bc7eba4892293b
jangamswarupa-595 <swarupajangam9@gmail.com>
2024-07-25 14:51:43 +0530

    Assignment6

--- ReturnArray.java
+++ ReturnArray.java
@@ -1,11 +1,17 @@
 public class ReturnArray{
   
 public int[] printarr(int[]a){
+  
   // initialize []a here
   return a;   
     }
   public static void main(String[]args){
   ReturnArray r=new ReturnArray();
     // call the function and print it here
+    int a[]={2,4,6,8,10};
+    r.printarr(a);
+    for (int k : a){
+     System.out.println(k);
   }
+}
 }

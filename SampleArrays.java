7451eda29ad5b9427d8cbca88bc3c8c15346f891
12Rameshwar <Chavhanrameshwar13@gmail.com>
2024-07-25 23:00:06 +0530

    Update SampleArrays.java

--- SampleArrays.java
+++ SampleArrays.java
@@ -1,4 +1,14 @@
-public class SampleArrays{
-// Declare and Initialize an Array
-  // print an array using for loop
+public class SampleArrays {
+
+    public static void main(String[] args) {
+        // Declare and initialize an array of integers
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Print the array elements using a for loop
+        System.out.println("Array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
 }
+

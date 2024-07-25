694549db4f10b688dbd1fc2d0eadf21d79920d15
12Rameshwar <Chavhanrameshwar13@gmail.com>
2024-07-25 22:57:35 +0530

    Update DynamicArrays.java

--- DynamicArrays.java
+++ DynamicArrays.java
@@ -1,5 +1,35 @@
-public class DynamicArrays{
-//create an array
-  // initialize an array using scanner and for loop
-  // iterate the values using for each loop
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DynamicArrays {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+    
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        
+        System.out.print("How many numbers would you like to input? ");
+        int n = scanner.nextInt();
+
+    
+        System.out.println("Please enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            int number = scanner.nextInt();
+            numbers.add(number);  
+        }
+
+        
+        System.out.println(" entered the following numbers:");
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+
+        
+        scanner.close();
+    }
 }
+
+

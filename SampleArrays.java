bab3b77bcfddaed7550967fe2cc2bdc1662a4f25
kavitha-guntru <kavithaguntru60@gmail.com>
2024-07-25 14:52:27 +0530

    kavitha

--- SampleArrays.java
+++ SampleArrays.java
@@ -1,4 +1,11 @@
 public class SampleArrays{
 // Declare and Initialize an Array
   // print an array using for loop
+  public static void main(String[]args){
+    int []no={1,2,3,4,5};
+      for(int i=0;i<no.length;i++){
+        // Fill out the remaining code
+        System.out.println(no[i]);
+    }
+  }
 }

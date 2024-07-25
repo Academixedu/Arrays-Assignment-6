3e58314282913a5e7dac17dcf52385bf67feecd9
Bipasa-mohapatra <mohapatrabipasa2002@gmail.com>
2024-07-25 15:02:09 +0530

    Assignment-6

--- SampleArrays.java
+++ SampleArrays.java
@@ -1,4 +1,10 @@
 public class SampleArrays{
-// Declare and Initialize an Array
+public static void main(String args[])
+{
+  int[] arr = {2,4,6,3,5};
+  for (int i = 0; i < arr.length; i++) {
+    System.out.println(arr[i]);// Declare and Initialize an Array
   // print an array using for loop
+}
+}
 }
